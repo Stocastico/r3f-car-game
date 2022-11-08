51e57f0d1f10e29902923c39e60ed955b078933e
Stefano Masneri <stocastico@gmail.com>
2022-11-08 19:36:23 +0100

    Add Car.jsx

---
+++ src/Car.jsx
@@ -0,0 +1,19 @@
+import { useLoader } from "@react-three/fiber";
+import { useEffect } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+export function Car () {
+    let mesh = useLoader(
+        GLTFLoader,
+        process.env.PUBLIC_URL + "/models/car.glb"
+    ).scene;
+
+    useEffect(() => {
+        mesh.scale.set(0.0012, 0.0012, 0.0012);
+        mesh.children[0].position.set(-365, -18, -65);
+    }, [mesh]);
+
+    return (
+        <primitive object={mesh} rotation-y={Math.PI} />
+    )
+}
